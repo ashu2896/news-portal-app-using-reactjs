a2876f4804483ce02224be7d3cece6930468efc7
ashu2896 <ashus6377@gmail.com>
2021-10-18 16:17:46 +0530

    initial commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+    <div className="jumbotron">
+        <h4>News Portal Application</h4>
+        <p>Assignment Project on React</p>
+    </div>
+);
+
+export default HomePage;
